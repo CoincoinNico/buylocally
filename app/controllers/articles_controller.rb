9e3bcd3093a5413367f071666367e6a6ca268ff9
Nicolas T <ntournaud@gmail.com>
2014-05-19 15:52:22 +0200

    Fixed before pull

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-before_action :set_article, only: [:update, :edit, :destroy, :show]
+  before_action :set_article, only: [:update, :edit, :destroy, :show]
 
   def index
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,31 @@
 
 ActiveRecord::Schema.define(version: 20140519114206) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "price"
     t.integer  "quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.string   "comment"
+    t.text     "body"
+    t.integer  "single_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["single_id"], name: "index_comments_on_single_id", using: :btree
+
+  create_table "singles", force: true do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.string   "sex"
+    t.string   "description"
+    t.integer  "note"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
