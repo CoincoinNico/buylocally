9383d9cae6f1556b84e26943fd4ce5cb3938759d
Nicolas T <ntournaud@gmail.com>
2014-05-20 14:01:24 +0200

    bundle install, all ready to push to heroku

--- app/models/picture.rb
+++
@@ -1,6 +0,0 @@
-class Picture < ActiveRecord::Base
-  belongs_to :article
-  has_attached_file :picture, :styles => { :small => “150×150>”, :large => “320×240>” }
-  # validates_attachment_presence :photo
-  # validates_attachment_size :photo, :less_than => 5.megabytes
-end
