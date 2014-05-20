0b7ba0fa7b4f1c07076cf166b1c011cee6d86c37
Nicolas T <ntournaud@gmail.com>
2014-05-20 16:12:45 +0200

    works locally

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,11 +1,11 @@
 class Asset < ActiveRecord::Base
   belongs_to :article
   has_attached_file :image,
-    :styles => {
-      :thumb=> "100x100#",
-      :small  => "300x300>",
-      :large => "600x600>"
-        }
+                    :styles => {
+                      :thumb=> "100x100#",
+                      :small  => "300x300>",
+                      :large => "600x600>"
+                    }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   # validates_attachment_presence :image
   # validates_attachment_size :image, :less_than => 5.megabytes
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,27 +1,27 @@
-defaults = {
+# defaults = {
 
- storage: :s3,
+#  storage: :s3,
 
- s3_credentials: {
+#  s3_credentials: {
 
- bucket: ENV['S3_BUCKET_NAME'],
+#  bucket: ENV['S3_BUCKET_NAME'],
 
- access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
 
- secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+#  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
 
- },
+#  },
 
- url: ':s3_domain_url',
+#  url: ':s3_domain_url',
 
- path: '/:rails_env/:class/:attachment/:id_partition/:style/:filename'
+#  path: '/:rails_env/:class/:attachment/:id_partition/:style/:filename'
 
-}
+# }
 
-!
+# !
 
-defaults.each do |key, value|
+# defaults.each do |key, value|
 
- Paperclip::Attachment.default_options[key] = value
+#  Paperclip::Attachment.default_options[key] = value
 
-endB
+# end
