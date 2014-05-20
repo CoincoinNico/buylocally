cd895536fd2f0d0fde55350cface7d751b2e12a4
Nicolas T <ntournaud@gmail.com>
2014-05-20 08:41:11 +0200

    added explanation of errors in form

--- app/models/article_image.rb
+++
@@ -1,6 +0,0 @@
-class ArticleImage < ActiveRecord::Base
-  belongs_to :article
-  has_attached_file :photo, :styles => { :small => “150×150>”, :large => “320×240>” }
-  validates_attachment_presence : photo
-  validates_attachment_size : photo, :less_than => 5.megabytes
-end
